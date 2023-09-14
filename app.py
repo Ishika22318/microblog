2e5663f9d62b6d19124505c68e0579b7b16b20ee
Ishika22318 <102206596+Ishika22318@users.noreply.github.com>
2023-09-14 19:12:23 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+import urllib.parse
+
+
+def create_app():
+    app = Flask(__name__)
+    username = urllib.parse.quote_plus("ishika22318")
+    password = urllib.parse.quote_plus("Ishu@210901")
+    client = MongoClient(
+        "mongodb+srv://%s:%s@microblog-application.piyqilm.mongodb.net/"
+        % (username, password)
+    )
+    app.db = client.microblog
+
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert_one(
+                {"content": entry_content, "date": formatted_date}
+            )
+
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d"),
+            )
+            for entry in app.db.entries.find({})
+        ]
+        return render_template("home.html", entries=entries_with_date)
+
+    return app
